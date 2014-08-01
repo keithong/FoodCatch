fe8933f0eb30d377c5ca3af0667bc962feba7d12
Keith Samson <k.samson@klab.com>
2014-08-01 15:54:23 +0800

    Removed some unused codes.

--- FoodCatch/JsonViewController.m
+++ FoodCatch/JsonViewController.m
@@ -127,18 +127,6 @@
     }
 }
 
-- (void)showProgressHUD
-{
-    [SVProgressHUD show];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [self fetchJson];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [SVProgressHUD dismiss];
-        });
-    });
-
-}
-
 - (void)fetchJson
 {
     [SVProgressHUD show];
